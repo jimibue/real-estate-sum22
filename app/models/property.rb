d5528c058ac1c554511df458ba7b6b4c4e3b3db8
James <jimibue@gmail.com>
2022-06-29 11:36:26 -0600

    bug fix city and state add to query

--- app/models/property.rb
+++ app/models/property.rb
@@ -5,7 +5,7 @@ class Property < ApplicationRecord
   # class method called on class
   # self here refers to the class
   def self.available
-    Property.find_by_sql("SELECT ag.first_name, ag.last_name, ag.email, p.id as property_id, p.beds, p.sq_ft, p.price,p.agent_id, p.baths, a.street, a.id as address_id
+    Property.find_by_sql("SELECT ag.first_name, ag.last_name, ag.email, p.id as property_id, p.beds, p.sq_ft, p.price,p.agent_id, p.baths,a.city, a.zip, a.street, a.id as address_id
       FROM properties AS p
       INNER JOIN addresses AS a ON p.id = a.property_id
       INNER JOIN agents AS ag ON ag.id = p.agent_id
